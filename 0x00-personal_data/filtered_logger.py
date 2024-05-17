9b63e8e6419a467e6ad29bdb0dfa85195ab6e8db
zaynom <ouaziz0626@gmail.com>
2024-05-17 02:49:09 +0100

    Task 3: Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,9 @@ import mysql.connector
 from typing import List
 
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
 def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str
 ) -> str:
@@ -33,9 +36,6 @@ class RedactingFormatter(logging.Formatter):
         return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
 
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
-
-
 def get_logger() -> logging.Logger:
     """Create a logger"""
     logger = logging.getLogger("user_data")
@@ -47,16 +47,16 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """Connect to secure database"""
-    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
-
-    return mysql.connector.connect(
-        user=username, password=password, host=host, database=db_name
+    username = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    host = os.environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    psw = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    db_name = os.environ.get("PERSONAL_DATA_DB_NAME")
+    conn = mysql.connector.connect(
+        host=host, database=db_name, user=username, password=psw
     )
+    return conn
 
 
 def main():
