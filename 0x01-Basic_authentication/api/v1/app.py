757cc4893a143f81a3f4add8250b21e4bb71d283
zaynom <ouaziz0626@gmail.com>
2024-05-21 00:17:57 +0100

    Task 1: Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -5,7 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS, cross_origin
 import os
 
 
@@ -16,9 +16,14 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """Not found handler"""
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Unauthorized handler"""
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,22 +5,32 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
+    """GET /api/v1/status
     Return:
-      - the status of the API
+        - the status of the API
     """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats/', strict_slashes=False)
+@app_views.route("/stats/", strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
+    """GET /api/v1/stats
     Return:
-      - the number of each objects
+        - the number of each objects
     """
     from models.user import User
+
     stats = {}
-    stats['users'] = User.count()
+    stats["users"] = User.count()
     return jsonify(stats)
+
+
+@app_views.route("/unauthorized")
+def raise_401():
+    """GET /api/v1/unauthorized
+    Riase:
+        - 401 error
+    """
+    abort(401)
