0eea5d1c4bbf7bd0bb1b71e68d8836bd00e5b00b
zaynom <ouaziz0626@gmail.com>
2024-05-21 11:19:48 +0100

    Task 7: Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,6 +9,7 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(
         self, authorization_header: str
     ) -> str:
+        """Extract the base64 part of the Authorization header"""
         if (
             authorization_header is None
             or not isinstance(authorization_header, str)
