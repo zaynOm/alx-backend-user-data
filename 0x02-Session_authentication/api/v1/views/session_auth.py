32af7413a83390a5bda15318212fa9aa95d4324d
zaynom <ouaziz0626@gmail.com>
2024-05-24 01:43:39 +0100

    Task 8: Logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -39,3 +39,15 @@ def login():
     res = jsonify(current_user.to_json())
     res.set_cookie(session_name, session_id)
     return res
+
+
+@app_views.route(
+    "/auth_session/logout", methods=["DELETE"], strict_slashes=False
+)
+def logout():
+    """Logout the user"""
+    from api.v1.app import auth
+
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify(), 200
