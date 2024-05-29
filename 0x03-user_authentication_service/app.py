df70ca55fc83c2b30eeff01310215869dbb247a7
zaynom <ouaziz0626@gmail.com>
2024-05-29 09:50:15 +0100

    Task 7: Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,16 +2,32 @@
 """Flask app entry point"""
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
 def index():
     """Welcome route"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    """Register a user"""
+    email = request.form["email"]
+    password = request.form["password"]
+    print(email)
+    print(password)
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": email, "message": "user created"})
 
 
 if __name__ == "__main__":
