5639d39c7c317d747b979526f5435f413212207d
zaynom <ouaziz0626@gmail.com>
2024-05-29 08:21:16 +0100

    Task 1: create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -27,7 +27,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
