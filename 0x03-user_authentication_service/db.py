f0566d5284fc429fae8fe53910401fa214e04718
zaynom <ouaziz0626@gmail.com>
2024-05-29 09:02:44 +0100

    Task 3: update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -45,3 +45,12 @@ class DB:
         except InvalidRequestError:
             raise
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update user data"""
+        user = self.find_user_by(id=user_id)
+        for k, v in kwargs.items():
+            if not hasattr(user, k):
+                raise ValueError
+            setattr(user, k, v)
+        self._session.commit()
