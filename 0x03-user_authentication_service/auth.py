1ee626fe5c36e70d55f7d9bd41d75c2173f2abd5
zaynom <ouaziz0626@gmail.com>
2024-05-30 17:57:00 +0100

    Task 16: Generate reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -63,3 +63,12 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """Destroy a session"""
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate reset password token"""
+        try:
+            user = self._db.find_user_by(email=email)
+            user.reset_token = _generate_uuid()
+            return user.reset_token
+        except NoResultFound:
+            raise ValueError
